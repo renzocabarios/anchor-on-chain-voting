e11b0dadf022751b4ace3c9d65a61c0e0bb88c59
renzocabarios <renzo.cabarios@gmail.com>
2023-09-01 16:36:23 +0800

    Add on chain

---
+++ tests/onchain_voting.ts
@@ -0,0 +1,48 @@
+import * as anchor from "@coral-xyz/anchor";
+import { Program } from "@coral-xyz/anchor";
+import { OnchainVoting } from "../target/types/onchain_voting";
+
+describe("onchain_voting", () => {
+  anchor.setProvider(anchor.AnchorProvider.env());
+  const program = anchor.workspace.OnchainVoting as Program<OnchainVoting>;
+  let voteBank = anchor.web3.Keypair.generate();
+
+  it("Creating vote bank for public to vote", async () => {
+    const tx = await program.methods
+      .initVoteBank()
+      .accounts({
+        voteAccount: voteBank.publicKey,
+      })
+      .signers([voteBank])
+      .rpc();
+    console.log("TxHash ::", tx);
+  });
+
+  it("Vote for GM", async () => {
+    const tx = await program.methods
+      .gibVote({ gm: {} })
+      .accounts({
+        voteAccount: voteBank.publicKey,
+      })
+      .rpc();
+    console.log("TxHash ::", tx);
+
+    let voteBankData = await program.account.voteBank.fetch(voteBank.publicKey);
+    console.log(`Total GMs :: ${voteBankData.gm}`);
+    console.log(`Total GNs :: ${voteBankData.gn}`);
+  });
+
+  it("Vote for GN", async () => {
+    const tx = await program.methods
+      .gibVote({ gn: {} })
+      .accounts({
+        voteAccount: voteBank.publicKey,
+      })
+      .rpc();
+    console.log("TxHash ::", tx);
+
+    let voteBankData = await program.account.voteBank.fetch(voteBank.publicKey);
+    console.log(`Total GMs :: ${voteBankData.gm}`);
+    console.log(`Total GNs :: ${voteBankData.gn}`);
+  });
+});
